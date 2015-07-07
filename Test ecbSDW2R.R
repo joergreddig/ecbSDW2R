ff7f01802f67bc7e6c33de0be0efe021f0c4b07c
joergreddig <joergreddig@gmail.com>
2015-07-07 23:04:30 +0200

    clean document, comments to documentation

---
+++ Test ecbSDW2R.R
@@ -0,0 +1,1 @@
+## Test ecbSDW2R
--- ecbSDW2R.R
+++ ecbSDW2R.R
@@ -2,7 +2,7 @@
 ## Joerg Reddig
 ## 21.4.2015
 ## ecbSDW2R
-## Last Change: 4.7.2015 
+## Last Change: 7.7.2015 
 ##===============================
 
 # Github: https://github.com/joergreddig/ecbSDW2R
@@ -11,29 +11,10 @@
 
 
 
-# TODO(JR): 
-# write function that retrieves data from SDW and stores it into a dataframe
-# function should allow for options from ECB SDW API
-# how to retrieve data from internal SDW? -> internal reference page
-# make a package on Github!
-# use wildcards!
-# decide between fix gesmes keys or individually assembled keys
-
-
-# have an internal and external version of a functino to download the data!
-
-
-#confidential data cannot be downloaded via the (internal) SDW API
-
-series.key <- "IVF.Q.U2.N.T0.A30.A.1.Z5.0000.Z01.E" 
-#IVF.Q.U2.N.T0.A5A.A.1.Z5.0000.Z01.E
-series.key <- "ICPF.Q.U.N.T0.A30.blabla"
-series.key <- "BSI.M.U2.Y.V.M30.X.I.U2.2300.Z01.A"
-
 # Load packages
 library(rsdmx)
 library(stringr)
-
+library(zoo)
 
 
 ## Function for cutting the series key into needed parts
@@ -59,14 +40,18 @@ split.series.key <- function(series.key) {
 
 # Construct the appropriate REST query.
 
-sdw.query <- function (series.key.elements) {
+sdw.query <- function (series.key.elements, internal) {
   
   dataflow    <- series.key.elements$dataflow
   key         <- series.key.elements$key
   resource    <- "data"
   
   # Construct the query
-  base_url    <- "http://sdw-wsrest.ecb.europa.eu/service/"
+  if(internal==TRUE){
+    base_url    <- "http://sdw-wsrest.ecb.europa.eu/service/" # Replace with internal url!
+  } else {
+    base_url    <- "http://sdw-wsrest.ecb.europa.eu/service/"
+  }
   partial_url <- paste(resource, dataflow, key, sep="/")
   rest_query  <- paste0(base_url, partial_url)
   
@@ -77,11 +62,11 @@ sdw.query <- function (series.key.elements) {
 
 
 
-get.sdw.series <- function(series.key) {
+get.sdw.series <- function(series.key, internal=FALSE) {
   
   series.key.elements <- split.series.key(series.key)
   
-  sdmx <- sdw.query(series.key.elements)
+  sdmx <- sdw.query(series.key.elements, internal)
   
   # Extract the numerical data 
   retrieved.data <- as.data.frame(sdmx)
@@ -92,54 +77,25 @@ get.sdw.series <- function(series.key) {
 
 
 
-
-colnames(stats)[2] <- paste(series.key)
-
-plot(stats)
+#=================
 
 
 
 
-series.keys <- c("IVF.Q.U2.N.10.A30.A.1.Z5.0000.Z01.E", "IVF.Q.U2.N.20.A30.A.1.Z5.0000.Z01.E", "IVF.Q.U2.N.20.A30.A.1.Z5.0000.Z01.E")
-
-
-
-for(i in series.keys){
-  get.sdw.series(i)
+as.time.series <- function(data, freq){
+  #determine whether data is monthly or quarterly frequency and convert data to the respective time series
+  if (freq=="m"){
+    # for monthly data, convert to date format: 
+    data$obsTime <-  as.Date(as.yearmon(data$obsTime), frac=1) # konvertiert 2015-01 nach date format 2015-01-31 
+  } else if (freq=="q"){
+    # for quarterly data to date: 
+    data$obsTime <-  as.Date(as.yearqtr(data$obsTime, format="%Y-Q%q"), frac=1) # sdw quarterly format: "%Y-Q%q"
+  }
   
-  #mehrere reihen herunterladen und all in einen data frame reinstecken mit dem Namen der series als colName. 
-  
+  return(data)
 }
 
 
 
-
-plot.ts(stats$obsValue)
-
-m3growth <- stats[ , c("obsTime", "obsValue")]
-
-
-ggplot(stats, aes(x=obsTime, y=obsValue)) + geom_line() + scale_x_date(format = "%b-%Y")
-
-
-
-library(zoo)
-stats$obsTime <- read.zoo(stats$obsTime, FUN = as.yearmon)
-
-stats$m3growth <- as.Date(stats$m3growth, format="%Y-%m")
-
-stats$obsValue <- as.Date(stats$obsValue, format="%Y-%m")
-
-m3 <- as.xts(m3growth)
-
-
-
-str(m3growth)
-
-stats1 <- as.data.frame(sdmx)
-
-head(stats1$obsvalue)
-
-# To be continued...
 
 # End of script.
